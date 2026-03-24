c1dd9afc66c40f1efc94b9c0b7fd83bba47764e0
Ibai Fernández <info@ibaifernandez.com>
2026-03-24 16:17:25 -0500

    feat: migrate remote assets to local Astro-optimized images for 100% technical parity

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -10,8 +10,8 @@ module.exports = {
     assert: {
       preset: 'lighthouse:recommended',
       assertions: {
-        'categories:performance': ['error', { minScore: 0.5 }],
-        'categories:accessibility': ['error', { minScore: 0.9 }],
+        'categories:performance': ['error', { minScore: 0.9 }],
+        'categories:accessibility': ['error', { minScore: 1.0 }],
         'categories:seo': ['error', { minScore: 0.9 }],
         'categories:best-practices': ['error', { minScore: 0.9 }],
       },
